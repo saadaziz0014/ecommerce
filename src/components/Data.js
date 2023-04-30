748438e902bba6fb5ed7a0c0facc8cb8cb9c68d5
saadaziz0014 <saadaziz0014@gmail.com>
2023-04-30 12:44:10 +0500

    done

---
+++ src/components/Data.js
@@ -0,0 +1,47 @@
+const Data = {
+  productItems: [
+    {
+      id: 1,
+      discount: 50,
+      cover: "./images/flash/flash-1.png",
+      name: "Solar Pannels",
+      price: 100,
+    },
+    {
+      id: 2,
+      discount: 40,
+      cover: "./images/flash/flash-2.png",
+      name: "Inverters",
+      price: 20,
+    },
+    {
+      id: 3,
+      discount: 40,
+      cover: "./images/flash/flash-3.png",
+      name: "Batteries",
+      price: 200,
+    },
+    {
+      id: 4,
+      discount: 40,
+      cover: "./images/flash/flash-4.png",
+      name: "Kits",
+      price: 50,
+    },
+    {
+      id: 5,
+      discount: 50,
+      cover: "./images/flash/flash-1.png",
+      name: "Wires",
+      price: 100,
+    },
+    {
+      id: 6,
+      discount: 50,
+      cover: "./images/flash/flash-3.png",
+      name: "Cleaner",
+      price: 100,
+    },
+  ],
+};
+export default Data;
